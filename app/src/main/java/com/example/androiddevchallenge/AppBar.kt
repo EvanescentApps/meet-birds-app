6e1f5aad21e0d5f8965c0a5d72de15ba1e62574f
ElectroInc <65493336+ElectroInc@users.noreply.github.com>
2021-02-27 21:10:00 +0100

    Fixed spotless issue

--- app/src/main/java/com/example/androiddevchallenge/AppBar.kt
+++ app/src/main/java/com/example/androiddevchallenge/AppBar.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example.androiddevchallenge
 
 import androidx.compose.foundation.Image
@@ -32,7 +31,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
 
 @Composable
 fun AppBar(
@@ -71,4 +69,3 @@ fun AppBar(
 fun AppBarPreview() {
     AppBar(title = { Text("Preview!") })
 }
-
