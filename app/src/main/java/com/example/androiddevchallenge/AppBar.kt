2af3f3f8edef71e712ceb1cbe3827e621ce8262a
ElectroInc <65493336+ElectroInc@users.noreply.github.com>
2021-02-27 20:30:14 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/androiddevchallenge/AppBar.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androiddevchallenge
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun AppBar(
+    modifier: Modifier = Modifier,
+    onNavIconPressed: () -> Unit = { },
+    title: @Composable RowScope.() -> Unit,
+    actions: @Composable RowScope.() -> Unit = {}
+) {
+    val backgroundColor = Color(0xFF3700B3)
+    Column(
+        Modifier.background(backgroundColor.copy(alpha = 0.95f))
+    ) {
+        TopAppBar(
+            modifier = modifier,
+            backgroundColor = Color.Transparent,
+            elevation = 0.dp,
+            contentColor = Color(0xFFFFA000),
+            actions = actions,
+            title = { Row { title() } },
+            navigationIcon = {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_meet_bird_icon),
+                    contentDescription = "AppBar Icon",
+                    modifier = Modifier
+                        .clickable(onClick = onNavIconPressed)
+                        .padding(horizontal = 16.dp)
+                )
+            }
+        )
+        Divider()
+    }
+}
+
+@Preview
+@Composable
+fun AppBarPreview() {
+    AppBar(title = { Text("Preview!") })
+}
+
---
+++ app/src/main/java/com/example/androiddevchallenge/DetailsActivity.kt
@@ -0,0 +1,91 @@
+package com.example.androiddevchallenge
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.ui.theme.AndroidDevChallengeTheme
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            DetailsContent()
+        }
+    }
+}
+
+@Composable
+fun DetailsContent() {
+    AndroidDevChallengeTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            MaterialTheme {
+                Column(
+                    modifier = Modifier.fillMaxHeight()
+                ) {
+
+                    Column(modifier = Modifier.fillMaxHeight()) {
+                        Column(modifier = Modifier.weight(1f)) {
+                            MyAppBar()
+
+                            val bird = Bird(
+                                "Robert, from Paris, loves repeating what you say",
+                                R.drawable.im2,
+                                "Paris, France",
+                                1
+                            )
+                            Image(
+                                painter = painterResource(bird.image),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                contentScale = ContentScale.Crop
+
+                            )
+                            Column(
+                                modifier = Modifier
+                                    .padding(16.dp, 16.dp, 16.dp, 0.dp)
+                            ) {
+                                NewsStory(bird)
+                                ProfileFab()
+                            }
+                            Text(
+                                "Robert is a colorful parrot able to repeat simple sentences. He loves watching old TV Series and he often repeats famous replicas. Robert is very funny, and he knows how to make you laugh, however he has the bad habit to wake you up at night, by shouting some of his favorite replicas.",
+                                modifier = Modifier.padding(16.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileFab(modifier: Modifier = Modifier) {
+
+    FloatingActionButton(
+        onClick = { },
+        modifier = modifier
+            .padding(16.dp)
+            .height(48.dp)
+            .width(250.dp),
+        content = { Text(text = "Meet Robert in a video call !") },
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = MaterialTheme.colors.onPrimary
+    )
+}
+
+
